a3ece54c05688db0a93ca5e373df8f5a9ab3b661
LISA FOSS <80625733+Lisafoss22@users.noreply.github.com>
2021-07-20 10:23:40 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id17034857_spank_bank','gR2Ten<4uBb9Isc@','id17034857_lisafoss');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
